15eea82fc3fc7d14859e3c0f1b62494867569eec
mosaad <mosaad@gmail.com>
2025-03-07 23:34:47 +0200

    first commit

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\UrlController;
 use Illuminate\Support\Facades\Route;
 
 
+
 Route::middleware('throttle:60,1')->group(function () {
     Route::post('/shorten', [UrlController::class, 'shorten']);
     Route::get('/{alias}', [UrlController::class, 'redirect'])->where('alias', '[A-Za-z0-9-]+');
